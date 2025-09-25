db5ae766b8e444d161b7cc98dcb6be479be1815a
Ethan Brewin <ebrewin@fairfields.co.uk>
2025-09-25 16:08:01 +0100

    RabbitMQ

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import amqplib from "amqplib/callback_api";
+
+amqplib.connect("amqp:localhost", (error0, connection) => {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel((error1, channel) => {
+    if (error1) {
+      throw error1;
+    }
+    const queue = "hello";
+    const msg = "Hello World!";
+
+    channel.assertQueue(queue, {
+      durable: false
+    });
+    channel.sendToQueue(queue, Buffer.from(msg));
+    console.log(" [x] Sent %s", msg);
+  });
+});
