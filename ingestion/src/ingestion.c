4a03e4c0bf950e5bf85e70cf52bf77e5eeb0d318
louie <c3066515@hallam.shu.ac.uk>
2025-09-28 12:56:52 +0100

    add ingestion.q message consumption and sigint handler

--- ingestion/src/ingestion.c
+++ ingestion/src/ingestion.c
@@ -1,5 +1,18 @@
 #include "ingestion.h"
 
+V SigIntHandler(S32 SigVal) {
+    SigIntReceived = TRUE;
+}
+
+U8 CheckRpcReply(AMQP_CONN Connection) {
+    AMQP_RPC_REP Ret;
+    Ret = amqp_get_rpc_reply(Connection);
+    if (Ret.reply_type != AMQP_RESPONSE_NORMAL) {
+        return 1;
+    }
+    return OK;
+}
+
 U8 InitiateConnection(AMQP_CONN* Connection) {
     AMQP_SOCK* Socket;
 
@@ -23,12 +36,30 @@ U8 InitiateConnection(AMQP_CONN* Connection) {
     }   
 
     {
-        enum amqp_response_type_enum_ Ret;
-        Ret = amqp_login(*Connection, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, "guest", "guest").reply_type;
-        if (Ret != AMQP_RESPONSE_NORMAL) {
+        AMQP_RPC_REP Ret;
+        Ret = amqp_login(*Connection, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, "guest", "guest");
+        if (Ret.reply_type != AMQP_RESPONSE_NORMAL) {
             fprintf(stderr, "Could not login to amqp");
             return 1;
         }
+    }
+
+    amqp_channel_open(*Connection, 1);
+    if (CheckRpcReply(*Connection) != OK) {
+        fprintf(stderr, "Could not open channel");
+        return 1;
+    }
+
+    amqp_queue_declare(*Connection, 1, amqp_cstring_bytes(AMQP_INGESTION_QUEUE_NAME), 0, 0, 0, 0, amqp_empty_table);
+    if (CheckRpcReply(*Connection) != OK) {
+        fprintf(stderr, "Could not declare queue");
+        return 1;
+    }
+        
+    amqp_basic_consume(*Connection, 1, amqp_cstring_bytes(AMQP_INGESTION_QUEUE_NAME), amqp_empty_bytes, 0, 1, 0, amqp_empty_table);
+    if (CheckRpcReply(*Connection) != OK) {
+        fprintf(stderr, "Could not consume queue");
+        return 1;
     }
     
     return OK;
@@ -40,6 +71,32 @@ V CloseConnection(AMQP_CONN* Connection) {
     amqp_destroy_connection(*Connection);
 }
 
+U8 IngestionMainloop(AMQP_CONN* Connection) {
+    AMQP_ENVEL Envelope;
+    AMQP_RPC_REP Ret;
+
+    while (SigIntReceived == FALSE) {
+        amqp_maybe_release_buffers(*Connection);
+
+        Ret = amqp_consume_message(*Connection, &Envelope, NULL, 0);
+        if (Ret.reply_type != AMQP_RESPONSE_NORMAL) {
+            fprintf(stderr, "Could not consume message");
+            return 1;
+        }
+
+        //ToDo process message
+        //ToDo publish to events topic
+        
+        amqp_destroy_envelope(&Envelope);
+    }
+    return OK;
+}
+
 U8 main(U8 argc, U8* argv[]) {
-    return 0;
+    SigIntReceived = FALSE;
+    if (signal(SIGINT, SigIntHandler) == SIG_ERR) {
+        fprintf(stderr, "Could not configure sigint handler");
+        return 1;
+    }
+    return OK;
 }
--- ingestion/src/types.h
+++ ingestion/src/types.h
@@ -9,8 +9,11 @@ typedef uint32_t U32;
 typedef int32_t S32;
 typedef float F32;
 typedef double F64;
+typedef void V;
+
+typedef amqp_envelope_t AMQP_ENVEL;
+typedef amqp_rpc_reply_t AMQP_RPC_REP;
 typedef amqp_connection_state_t AMQP_CONN;
 typedef amqp_socket_t AMQP_SOCK;
-typedef void V;
 
 #endif
