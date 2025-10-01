5482d63e4c198a03b6a23404652cd2550c698249
louie <c3066515@hallam.shu.ac.uk>
2025-10-01 21:48:23 +0100

    connect to rabbitmq and teardown on sigint successfully

--- ingestion/src/ingestion.c
+++ ingestion/src/ingestion.c
@@ -1,5 +1,8 @@
 #include "ingestion.h"
 
+//globals
+volatile sig_atomic_t SigIntReceived;
+
 V SigIntHandler(S32 SigVal) {
     SigIntReceived = TRUE;
 }
@@ -13,7 +16,7 @@ U8 CheckRpcReply(AMQP_CONN Connection) {
     return OK;
 }
 
-U8 InitiateConnection(AMQP_CONN* Connection, U8* IP, S32 Port) {
+U8 InitiateConnection(AMQP_CONN* Connection, U8* IP, S32 Port, U8* Username, U8* Password) {
     AMQP_SOCK* Socket;
 
     *Connection = amqp_new_connection();
@@ -27,17 +30,17 @@ U8 InitiateConnection(AMQP_CONN* Connection, U8* IP, S32 Port) {
     }
 
     {
-        U8 Ret;
+        S32 Ret;
         Ret = amqp_socket_open(Socket, IP, Port);
         if (Ret != OK) {
-            fprintf(stderr, "Could not open tcp socket");
+            fprintf(stderr, "Could not open tcp socket at %s:%d, %s", IP, Port, amqp_error_string2(Ret));
             return 1;
         }
     }   
 
     {
         AMQP_RPC_REP Ret;
-        Ret = amqp_login(*Connection, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, "guest", "guest");
+        Ret = amqp_login(*Connection, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, Username, Password);
         if (Ret.reply_type != AMQP_RESPONSE_NORMAL) {
             fprintf(stderr, "Could not login to amqp");
             return 1;
@@ -74,13 +77,16 @@ V CloseConnection(AMQP_CONN* Connection) {
 U8 IngestionMainloop(AMQP_CONN* Connection) {
     AMQP_ENVEL Envelope;
     AMQP_RPC_REP Ret;
+    struct timeval Timeout = {0};
+
+    Timeout.tv_sec = 1;
 
     while (SigIntReceived == FALSE) {
         amqp_maybe_release_buffers(*Connection);
 
-        Ret = amqp_consume_message(*Connection, &Envelope, NULL, 0);
-        if (Ret.reply_type != AMQP_RESPONSE_NORMAL) {
-            fprintf(stderr, "Could not consume message");
+        Ret = amqp_consume_message(*Connection, &Envelope, &Timeout, 0);
+        if (Ret.reply_type != AMQP_RESPONSE_NORMAL && Ret.library_error != AMQP_STATUS_TIMEOUT) {
+            fprintf(stderr, "Could not consume message, %d", Ret.reply_type);
             return 1;
         }
 
@@ -97,12 +103,23 @@ U8 main(U8 argc, U8* argv[]) {
     S32 Port = 0;
     U8 RabbitMQUsername[64] = {0};
     U8 RabbitMQPassword[64] = {0};
+    AMQP_CONN Conn = {0};
 
     SigIntReceived = FALSE;
-    if (0&&signal(SIGINT, SigIntHandler) == SIG_ERR) {
+    if (signal(SIGINT, SigIntHandler) == SIG_ERR) {
         fprintf(stderr, "Could not configure sigint handler");
         return 1;
     }
-    ReadRabbitConfig(IP, &Port, RabbitMQUsername, RabbitMQPassword);
+    if (ReadRabbitConfig(IP, &Port, RabbitMQUsername, RabbitMQPassword) == OK) {
+        fprintf(stdout, "read config\n");
+        if (InitiateConnection(&Conn, IP, Port, RabbitMQUsername, RabbitMQPassword) == OK) {
+            fprintf(stdout, "connected to rabbitmq\n");
+            if (IngestionMainloop(&Conn) == OK) {
+                CloseConnection(&Conn);
+                fprintf(stdout, "disconnected from rabbitmq\n");
+            }
+        }
+    }
+    
     return OK;
 }
