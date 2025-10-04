c5a82b26a82ba108378865b86558ceb0123f8c3f
louie <c3066515@hallam.shu.ac.uk>
2025-10-04 23:13:16 +0100

    hmac verify with test secret key

--- ingestion/src/ingestion.h
+++ ingestion/src/ingestion.h
@@ -7,18 +7,21 @@
 #include <signal.h>
 #include <string.h>
 #include <stdlib.h>
+#include <openssl/hmac.h>
+#include <openssl/evp.h>
 #include <sys/time.h>
+#include "cJSON.h"
 #include "types.h"
 
-//#defines
 #define AMQP_INGESTION_QUEUE_NAME "ingestion.q"
 #define TRUE 1
 #define FALSE 0
 #define OK 0
+#define NOK 1
 #define strtok_r __strtok_r
 
-//prototypes
 static V SigIntHandler(S32 SigVal);
 U8 ReadRabbitConfig(U8* IP, S32* Port, U8* Username, U8* Password);
+U8 HmacVerify(cJSON* MsgJson);
 
 #endif
