2ad0fd9f8f12dd0da8fbf2bad8aa8fc1d89a1d83
Ethan <ethan.brewin31@gmail.com>
2025-10-13 23:50:47 +0100

    uncomment RabbitMQ client

--- client/apps/server/src/index.ts
+++ client/apps/server/src/index.ts
@@ -1,50 +1,50 @@
-// import type { Publisher } from "rabbitmq-stream-js-client";
-// import { RabbitMQClient } from "./components/rabbitClient";
-// import { generateRandomNumber } from "./components/randomNumberGen";
-// import { rabbitMessage } from "@client/config/src/message";
-// import { createHmacSignature } from "./components/hmac";
+import type { Publisher } from "rabbitmq-stream-js-client";
+import { RabbitMQClient } from "./components/rabbitClient";
+import { generateRandomNumber } from "./components/randomNumberGen";
+import { rabbitMessage } from "@client/config/src/message";
+import { createHmacSignature } from "./components/hmac";
 import { server, port } from "./client/webSocket";
 
-// const rabbitInstance = RabbitMQClient.Instance;
-// const client = await rabbitInstance.connectionClient();
+const rabbitInstance = RabbitMQClient.Instance;
+const client = await rabbitInstance.connectionClient();
 
 // create 12 streams
-// for (let i = 0; i <= 11; i++) {
-//   await rabbitInstance.createStream(client, `streamtest${i}`);
-// }
+for (let i = 0; i <= 11; i++) {
+  await rabbitInstance.createStream(client, `streamtest${i}`);
+}
 
-// const publisherArray: Publisher[] = []
+const publisherArray: Publisher[] = []
 
 // create 12 publishers
-// for (let i = 0; i <= 11; i++) {
-//   const tempPublisher = await rabbitInstance.createPublisher(client, `streamtest${i}`);
-//   if (!tempPublisher) break;
-//   publisherArray.push(tempPublisher);
-// }
+for (let i = 0; i <= 11; i++) {
+  const tempPublisher = await rabbitInstance.createPublisher(client, `streamtest${i}`);
+  if (!tempPublisher) break;
+  publisherArray.push(tempPublisher);
+}
 
-// const message: rabbitMessage = {
-//   clientID: "client-0",
-//   currentReading: 43,
-//   unix: Math.floor(Date.now() / 1000),
-//   fwVersion: "1.0.0",
-//   unit: "kWh",
-//   signature: "HMAC-SHA256",
-// }
+const message: rabbitMessage = {
+  clientID: "client-0",
+  currentReading: 43,
+  unix: Math.floor(Date.now() / 1000),
+  fwVersion: "1.0.0",
+  unit: "kWh",
+  signature: "HMAC-SHA256",
+}
 
-// setInterval(async () => {
-//   const randomNum = generateRandomNumber(12);
-//   const currentPub = publisherArray.at(randomNum)
-//   if (!currentPub)
-//   {
-//     console.error("No publisher found");
-//     return;
-//   }
-//   message.clientID = `client-${randomNum + 1}`;
-//   message.currentReading = await rabbitInstance.readingHandler(randomNum + 1);
-//   message.unix = Math.floor(Date.now() / 1000);
-//   message.signature = createHmacSignature(message.clientID, message.currentReading.toString(), message.unix.toString());
-//   await rabbitInstance.messagehandler(currentPub, message);
-// }, 5000)
+setInterval(async () => {
+  const randomNum = generateRandomNumber(12);
+  const currentPub = publisherArray.at(randomNum)
+  if (!currentPub)
+  {
+    console.error("No publisher found");
+    return;
+  }
+  message.clientID = `client-${randomNum + 1}`;
+  message.currentReading = await rabbitInstance.readingHandler(randomNum + 1);
+  message.unix = Math.floor(Date.now() / 1000);
+  message.signature = createHmacSignature(message.clientID, message.currentReading.toString(), message.unix.toString());
+  await rabbitInstance.messagehandler(currentPub, message);
+}, 5000)
 
 server.listen(port, () => {
   console.log("Server is running on localhost:3000");
