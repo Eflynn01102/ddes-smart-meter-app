27ae09ebbd28aed7b3df99ccc28ecbec3ceb7972
Ethan <ethan.brewin31@gmail.com>
2025-10-25 11:42:19 +0100

    add PrimeVue

--- client/apps/client/src/main.ts
+++ client/apps/client/src/main.ts
@@ -1,12 +1,21 @@
 import "./assets/main.css";
 
 import { createApp } from "vue";
+import PrimeVue from "primevue/config";
 import { createPinia } from "pinia";
+
+import lara from '@primeuix/themes/lara';
 
 import App from "./App.vue";
 import router from "./router";
 
-const app = createApp(App);
+const app = createApp(App, {
+  theme: {
+    preset: lara
+  }
+});
+
+app.use(PrimeVue);
 
 app.use(createPinia());
 app.use(router);
