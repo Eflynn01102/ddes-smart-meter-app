8e0c718565ef11530485507fc2b49e8311ca3d1f
Ethan <ethan.brewin31@gmail.com>
2025-11-21 22:21:36 +0000

    it works!!!!!!!

--- client/apps/client/vite.config.ts
+++ client/apps/client/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			"/socket.io": {
-				target: "http://localhost:3000/",
+				target: "http://client-server:3000/",
 				ws: true,
 				changeOrigin: true,
 			},
